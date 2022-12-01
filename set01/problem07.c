37b2ebb7b3b97fe272953cf07f704450fa9d3845
ishita04 <114482950+ishitaaa2004@users.noreply.github.com>
2022-12-01 18:46:32 +0000

    done

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int input()
+{
+ int v;
+  printf("enter the numbers");
+   if( scanf("%d", &v)!=1)
+   {
+     printf("error");
+     exit(0);
+   }
+  return v;
+}
+int sum(int n)
+{
 
+ n=(n*(n+1))/2;//use for loop
+  return n;
+
+}
+
+void output(int n, int add)
+{
+ printf("sum of n numbers is %d", add); //1+2+3+...+n = sum.
+}
+
+int main()
+{int a=input();
+  int c;
+   c=sum(a);
+ output(a,c);
+ return 0;
+}  
