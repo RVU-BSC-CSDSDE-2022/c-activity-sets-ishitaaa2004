971b3aeab473e7df1435932b9d9f89f61ebb6f06
ishita04 <114482950+ishitaaa2004@users.noreply.github.com>
2022-12-01 18:29:31 +0000

    done

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,10 +1,1 @@
-#include<stdio.h>
-int main()
-{
-  int i=1;
-  for(;;)
-    {
-      printf("%d",i);
-    }  
-  return 0;
-    }
+
---
+++ set01/problem08.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include <stdlib.h>
+int input_array_size()
+{
+  int a; 
+  printf("enter the total number of values entered");
+  if(scanf("%d", &a)!=1)
+  {
+    printf("error");
+    exit(0);
+  }
+  return a;
+}
+void input(int n, int arr[n])
+{
+  printf("enter the values");
+  for(int i=0;i<n;i++)
+    {
+    scanf("%d", &arr[i]);
+    }
+  
+}
+int sum(int n,int arr[n])
+{
+  int sum=0;
+  for(int i=0;i<n;i++)
+    {
+      sum=sum+(int)arr[i];
+    }
+  return sum;
+}
+void output( int n, int arr[n],int sum)
+{
+  printf("%d",sum);
+}
+int main()
+{
+  int p=input_array_size();
+  int b[p];
+  input(p,b);
+  int r=sum(p,b);
+  output(p,b,r);
+  return 0;
+}
